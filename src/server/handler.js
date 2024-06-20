fbc8cfe90c7cc8bfb5eb09547f16a093c56de425
kurniawands <kds.wawan@gmail.com>
2024-06-21 04:49:29 +0700

    add edit profile handler

--- src/server/handler.js
+++ src/server/handler.js
@@ -1,20 +1,29 @@
 const InputError = require("../exceptions/InputError");
 const uploadImage = require("../services/uploadImage");
-const storeData = require("../services/storeData");
-const getUserData = require("../services/getUserData");
+const {
+  postUserData,
+  getUserData,
+  editUserData,
+} = require("../services/storeData");
 
-async function postUploadHandler(req, res, next) {
+async function postProfileHandler(req, res, next) {
   try {
     if (!req.file) {
-      next(new InputError(`Terjadi kesalahan input: Tidak ada file.`));
+      next(new InputError("Input Error: Tidak ada foto."));
+      return;
     }
+    if (!req.body.userId || !req.body.name) {
+      next(new InputError("Input Error: Data tidak lengkap."));
+      return;
+    }
+
     const image = req.file.buffer;
     const userId = req.body.userId;
     const name = req.body.name;
 
     const publicUrl = await uploadImage(image, userId, next);
 
-    await storeData(userId, name, publicUrl);
+    await postUserData(userId, name, publicUrl);
 
     res.status(201).send({
       status: "success",
@@ -25,13 +34,13 @@ async function postUploadHandler(req, res, next) {
   }
 }
 
-async function postProfileHandler(req, res, next) {
+async function getProfileHandler(req, res, next) {
   try {
     const userId = req.params.userId;
 
     const data = await getUserData(userId);
 
-    if (data === undefined) {
+    if (!data) {
       res.status(404).send({
         status: "fail",
         message: "User not found.",
@@ -48,4 +57,41 @@ async function postProfileHandler(req, res, next) {
   }
 }
 
-module.exports = { postUploadHandler, postProfileHandler };
+async function editProfileHandler(req, res, next) {
+  try {
+    const userId = req.params.userId;
+    if (!req.body.name && !req.file) {
+      next(new InputError("Input Error: Tidak ada data yang diubah."));
+      return;
+    }
+
+    let data;
+    if (req?.file) {
+      const image = req.file.buffer;
+      const publicUrl = await uploadImage(image, userId, next);
+      data = { ...data, pictureUrl: publicUrl };
+    }
+    if (req.body?.name) {
+      const name = req.body.name;
+      data = { ...data, name: name };
+    }
+
+    const check = await editUserData(userId, data);
+
+    if (!check) {
+      res.status(404).send({
+        status: "fail",
+        message: "User not exist",
+      });
+    } else {
+      res.status(200).send({
+        status: "success",
+        message: "User profile edited",
+      });
+    }
+  } catch (err) {
+    next(err);
+  }
+}
+
+module.exports = { postProfileHandler, getProfileHandler, editProfileHandler };
